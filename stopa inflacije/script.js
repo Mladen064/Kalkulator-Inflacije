00107c2ae18939d86668739868ebdf8d0b3aff69
Mladen <116673677+Mladen064@users.noreply.github.com>
2022-12-20 19:15:11 +0100

    Add files via upload

---
+++ stopa inflacije/script.js
@@ -0,0 +1,27 @@
+function inflationCalculator()
+{
+    let inflacija=document.getElementById('inflationRate');
+    let para=document.getElementById('money');
+    let godine=document.getElementById('years');
+
+    inflacija=parseFloat(inflationRate.value);
+    para=parseFloat(money.value);
+    godine=parseFloat(years.value);
+
+    let konacnaInflacija = para + (para * ( inflacija/100 ));
+
+    
+
+    for(let i=1; i<godine; i++)
+    {
+        konacnaInflacija +=konacnaInflacija * (inflacija/100);
+    }
+
+    konacnaInflacija=konacnaInflacija.toFixed(2);
+
+    let newElement = document.createElement('div');
+    newElement.className = 'new-value';
+    newElement.innerText ='Danasnjih ${para} vredi isto kao ${konacnaInflacija} za ${godina} godina';
+
+    document.querySelector('.container').appendChild(newElement);
+}
